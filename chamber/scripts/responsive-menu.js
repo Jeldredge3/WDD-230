cc32dbc4ab224a8fce9fac13a8fd3e2d7e3b4441
jeldredge3 <jordanmeldredge@gmail.com>
2023-01-25 10:56:17 -0700

    Week03 final updates

--- chamber/scripts/responsive-menu.js
+++ chamber/scripts/responsive-menu.js
@@ -2,11 +2,10 @@
 
 const menuButton = document.querySelector("#nav-button");
 const menuIcon = document.querySelector("#nav-icon");
-const menuItems = document.querySelectorAll(".nav-item");
+const menuItems = document.querySelectorAll(".nav-li");
 
 menuButton.addEventListener("click", () => {
 	menuItems.forEach((item) => item.classList.toggle("open"));
-	menuButton.classList.toggle("closed");
 	menuButton.classList.toggle("active");
 	menuIcon.classList.toggle("fa-bars");
 	menuIcon.classList.toggle("fa-x");
